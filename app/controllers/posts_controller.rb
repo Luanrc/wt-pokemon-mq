d925e8b23624b6609586086e0c16ff92892ee798
Luan <luan.rodrigues.c@gmail.com>
2022-04-25 10:07:40 -0300

    feat: setting rabbitmq to publisher

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,8 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
+        service = PublisherService.new()
+        service.send("posts", @post.attributes)
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
---
+++ app/services/publisher_service.rb
@@ -0,0 +1,18 @@
+class PublisherService
+  def initialize
+    @connection = Bunny.new(hostname: 'rabbitmq')
+  end
+  def send(exchange, message = {})
+    event = channel.fanout("world_trader.#{exchange}") 
+
+    event.publish(message.to_json)
+  end
+
+  def channel
+    @channel = connection.create_channel
+  end
+
+  def connection
+    @connection.start
+  end
+end
