635e8bba0843ef8e346fa8dd42240fcf76dbaec5
Luan <luan.rodrigues.c@gmail.com>
2022-04-26 16:59:10 -0300

    feat: improve publisher service

--- app/services/publisher_service.rb
+++ app/services/publisher_service.rb
@@ -1,20 +1,21 @@
 class PublisherService
   def initialize
     @connection = Bunny.new(hostname: 'rabbitmq')
+    @channel = connection.create_channel
   end
-  def send(message = {})
-    exchange = channel.fanout("world_trader_ping", durable: true)
 
+  def send(message)
+    
+    exchange = @channel.fanout("world_trader_ping", durable: true)
+    
+    puts '--------------enviou os dados para o ping-----------------------------'
+    puts "Dados enviados para o ping: #{message}"
     exchange.publish(message.to_json)
 
     @connection.close
   end
 
   private
-
-  def channel
-    connection.create_channel
-  end
 
   def connection
     @connection.start
